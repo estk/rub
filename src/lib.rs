62da445489c2396eb9520d914e40736cbd0a7293
Evan Simmons <evan.simmons@creditkarma.com>
2018-03-12 14:05:26 -0700

    It compiles

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,17 @@ extern crate hyper;
 extern crate hyper_tls;
 extern crate tokio_core;
 
+use std::rc::Rc;
+use std::cell::RefCell;
 use std::error::Error;
+use tokio_core::reactor::Core;
+use tokio_core::reactor::Remote;
 use futures::prelude::*;
 use futures::Future;
 use futures::sync::mpsc;
 use hyper::Client;
 use hyper::client::HttpConnector;
 use hyper_tls::HttpsConnector;
-use tokio_core::reactor::Core;
-use tokio_core::reactor::Remote;
 
 #[derive(Debug)]
 pub struct Config {
@@ -21,8 +23,6 @@ pub struct Config {
     pub concurrency: u16,
 }
 
-// clients: Vec<Client<HttpsConnector<HttpConnector>>>,
-// todo maybe try ^^
 impl Config {}
 
 pub fn run(config: Config) -> Result<(), Box<Error>> {
@@ -41,9 +41,6 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 
     let (tx, rx) = mpsc::channel(1);
 
-    for _ in 0..concurrency {
-        handle.spawn(|_| client.get(uri).then(|res| tx.send(res)))
-    }
     let rubber = Rubber {
         uri,
         resultsTx: tx,
@@ -53,7 +50,7 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
         running: 0,
         number: number,
         finished: 0,
-        results: vec![],
+        results: Rc::new(RefCell::new(vec![])),
     };
 
     let results = core.run(rubber)?;
@@ -62,36 +59,42 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
     Ok(())
 }
 
+type Results = Rc<RefCell<Vec<Result<hyper::Response<hyper::Body>, hyper::Error>>>>;
+
 struct Rubber {
     uri: hyper::Uri,
     resultsTx: mpsc::Sender<Result<hyper::Response<hyper::Body>, hyper::Error>>,
     resultsRx: mpsc::Receiver<Result<hyper::Response<hyper::Body>, hyper::Error>>,
     remote: Remote,
-    client: Client<hyper_tls::HttpsConnector<hyper::client::HttpConnector>>,
+    client: Client<hyper_tls::HttpsConnector<HttpConnector>>,
     running: u16,
     number: u32,
     finished: u32,
-    results: Vec<hyper::Response<hyper::Body>>,
+    results: Results,
 }
 
 impl Future for Rubber {
-    type Item = Vec<hyper::Response<hyper::Body>>;
-    type Error = hyper::Error;
-    fn poll(&mut self) -> Result<Async<Vec<hyper::Response<hyper::Body>>>, hyper::Error> {
+    type Item = Results;
+    type Error = Box<Error>;
+    fn poll(&mut self) -> Result<Async<Results>, Box<Error>> {
         loop {
-            let res = try_ready!(self.resultsRx.poll()).unwrap()?;
-            self.results.push(res);
+            let res = match self.resultsRx.poll() {
+                Ok(Async::NotReady) => return Ok(Async::NotReady),
+                Ok(Async::Ready(Some(v))) => v,
+                Ok(Async::Ready(None)) => return Ok(Async::Ready(self.results.clone())),
+                Err(_) => continue,
+            };
+            self.results.borrow_mut().push(res);
 
             let doneSpawning = self.running as u32 + self.finished >= self.number;
             if !doneSpawning {
-                self.remote.spawn(|_| {
-                    self.client
-                        .get(self.uri)
-                        .then(|res| self.resultsTx.send(res))
-                })
+                self.client.get(self.uri.clone()).then(|res| {
+                    let tx = &mut self.resultsTx;
+                    tx.send(res)
+                });
             }
             if self.finished >= self.number {
-                return Ok(Async::Ready(self.results));
+                return Ok(Async::Ready(self.results.clone()));
             }
         }
     }
