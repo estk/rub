c53b0a792882bf63a927a9684211af95e0de2f0e
Evan Simmons <evan.simmons@creditkarma.com>
2018-03-13 19:06:53 -0700

    Factor

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#[macro_use]
 extern crate futures;
 extern crate hyper;
 extern crate hyper_tls;
@@ -11,11 +10,8 @@ use tokio_core::reactor::Core;
 use tokio_core::reactor::Remote;
 use futures::prelude::*;
 use futures::Future;
-use futures::future::Executor;
-use futures::future::ok;
 use futures::sync::mpsc;
 use hyper::Client;
-use hyper::client::HttpConnector;
 use hyper_tls::HttpsConnector;
 
 #[derive(Debug)]
@@ -25,50 +21,34 @@ pub struct Config {
     pub concurrency: u16,
 }
 
-impl Config {}
-
 pub fn run(config: Config) -> Result<(), Box<Error>> {
     let mut core = Core::new()?;
+    let (results_tx, results_rx) = mpsc::channel(1);
 
-    let uri = config.url.parse()?;
-    let concurrency = config.concurrency;
-    let number = config.number;
+    let rubber = Rubber {
+        uri: config.url.parse()?,
+        number: config.number,
+        concurrency: config.concurrency,
+        running: 0,
+        finished: 0,
+        remote: core.remote(),
+        results_tx,
+        results_rx,
+        results: Arc::new(RefCell::new(vec![])),
+    };
 
     println!("running core");
 
-    let (tx, rx) = mpsc::channel(1);
-
-    let rubber = Rubber {
-        uri,
-        results_tx: tx,
-        results_rx: rx,
-        remote: core.remote(),
-        running: 0,
-        number: number,
-        finished: 0,
-        results: Arc::new(RefCell::new(vec![])),
-        concurrency: concurrency,
-    };
-
     let results = core.run(rubber)?;
     let unwrapped = Arc::try_unwrap(results).unwrap().into_inner();
-    let mut count = 0;
 
-    for _ in unwrapped.into_iter() {
-        count += 1;
-    }
+    let count = unwrapped.into_iter().fold(0, |acc, _| acc + 1);
 
-    // results.to_iter().for_each(|_| counter += 1;);
-    // results
-
-    println!("count: {:?}", count);
-
-    println!("finished running");
+    println!("finished requests: {:?}", count);
     Ok(())
 }
 
 type Results = Vec<Result<hyper::Response<hyper::Body>, hyper::Error>>;
-
 struct Rubber {
     uri: hyper::Uri,
     results_tx: mpsc::Sender<Result<hyper::Response<hyper::Body>, hyper::Error>>,
@@ -80,6 +60,31 @@ struct Rubber {
     results: Arc<RefCell<Results>>,
     concurrency: u16,
 }
+impl Rubber {
+    fn spawn_requests(&mut self) {
+        let done_spawning = self.running as u32 + self.finished >= self.number;
+        if !done_spawning {
+            for _ in 0..(self.concurrency - self.running) {
+                self.spawn_request();
+            }
+        }
+    }
+    fn spawn_request(&mut self) {
+        let remote = &mut self.remote;
+        let results_tx = self.results_tx.clone();
+        let uri = self.uri.clone();
+
+        remote.spawn(move |handle| {
+            let conn = HttpsConnector::new(4, &handle).unwrap();
+            let client = Client::configure().connector(conn).build(&handle);
+
+            client
+                .get(uri)
+                .then(|res| results_tx.send(res).then(|_| Ok(())))
+        });
+        self.running += 1;
+    }
+}
 
 impl Future for Rubber {
     type Item = Arc<RefCell<Results>>;
@@ -89,25 +94,8 @@ impl Future for Rubber {
             if self.finished >= self.number {
                 return Ok(Async::Ready(self.results.clone()));
             }
+            self.spawn_requests();
 
-            let done_spawning = self.running as u32 + self.finished >= self.number;
-            if !done_spawning {
-                for _ in 0..(self.concurrency - self.running) {
-                    let remote = &mut self.remote;
-                    let results_tx = self.results_tx.clone();
-                    let uri = self.uri.clone();
-
-                    remote.spawn(move |handle| {
-                        let conn = HttpsConnector::new(4, &handle).unwrap();
-                        let client = Client::configure().connector(conn).build(&handle);
-
-                        client
-                            .get(uri)
-                            .then(|res| results_tx.send(res).then(|_| Ok(())))
-                    });
-                    self.running += 1;
-                }
-            }
             let res = match self.results_rx.poll() {
                 Ok(Async::NotReady) => return Ok(Async::NotReady),
                 Ok(Async::Ready(Some(v))) => v,
